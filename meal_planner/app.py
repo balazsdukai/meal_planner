7318ab0dd9be1097d2ae6b1707926c42e3c1357f
Balázs Dukai <balazs.dukai@gmail.com>
2019-06-16 19:46:58 +0200

    Add and remove ingredients to a recipe (with JS)

--- meal_planner/app.py
+++ meal_planner/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm, Form
 from wtforms import StringField, TextAreaField, DecimalField, SelectField, FieldList, FormField, SubmitField
 from wtforms.validators import DataRequired
 from flask_wtf.csrf import CSRFProtect
@@ -9,14 +9,15 @@ app.config.from_object(__name__)
 app.secret_key = '04d8518b-110d-459c-ad3a-2c44cfb6419a'
 csrf = CSRFProtect(app)
 
-class AddIngredientForm(FlaskForm):
-    ingredient_name = StringField()
+class AddIngredientForm(Form):
+    units = [('',''),('tbsp', 'tbsp'), ('l', 'l'), ('kg', 'kg'), ('cup', 'cup')]
+    ingredientname = StringField()
     amount = DecimalField('amount')
-    unit = SelectField('unit', choices=[('tbsp', 'tbsp'), ('l', 'l'), ('kg', 'kg'), ('cup', 'cup')])
-    remove = SubmitField(label='Remove')
+    unit = SelectField('unit', choices=units)
 
 class AddIngredientsForm(FlaskForm):
-    ingredient_list = FieldList(FormField(AddIngredientForm), min_entries=1)
+    units = [('',''),('tbsp', 'tbsp'), ('l', 'l'), ('kg', 'kg'), ('cup', 'cup')]
+    ingredientlist = FieldList(FormField(AddIngredientForm), min_entries=1)
     save = SubmitField(label='Save')
     add_ingredient = SubmitField(label='Add ingredient')
     method = TextAreaField('method')
@@ -40,18 +41,11 @@ def recipe(recipe_name):
 
 @app.route("/add_recipe", methods=('GET', 'POST'))
 def add_recipe():
-    ingredients = []
-    form=AddIngredientsForm(ingredients=ingredients)
+    form=AddIngredientsForm()
 
-    if request.method == 'POST':
-        if form.add_ingredient.data:
-            form.ingredient_list.append_entry()
-        elif form.save.data:
-            print("Saving form")
-        else:
-            for i, ingredient in enumerate(form.ingredient_list):
-                if ingredient.remove.data:
-                    print("% should be deleted" % ingredient)
+    if form.validate_on_submit():
+        print(form.data)
+        return render_template('add_recipe.html', form=form, data=form.data)
 
     #     # NewItem = Item(name=request.form['name'], description=request.form['description'], price=request.form['price'], category_id = category_id, user_id=category.user_id)
     #     # session.add(NewItem)
