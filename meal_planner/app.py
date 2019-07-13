df3367d483550d5828eb5c88631811b4cae0cfa0
Balázs Dukai <balazs.dukai@gmail.com>
2019-07-13 13:35:39 +0200

    Connect recipe list to DB

--- meal_planner/app.py
+++ meal_planner/app.py
@@ -43,22 +43,19 @@ class AddIngredientsForm(FlaskForm):
     recipename = StringField()
     nr_meals = IntegerField()
 
-def get_recipes():
-    recipes = [(1,'salty soup'), (2,'hummus with hummus sauce'), (3,'tasty pie'),
-    (4,'this is very long names recepie with many ingredients'), (5,'blaa bla blaa')]
-    return recipes
 @app.route("/")
 def index():
     return render_template('index.html')
 
 @app.route("/recipes")
 def recipes():
-    recipe_list = get_recipes()
+    recipe_list = [(recipe.id, recipe.name) for recipe in Recipe.query.all()]
     return render_template('recipe_list.html', recipes=recipe_list)
 
-@app.route("/recipes/<string:recipe_name>/", methods=['GET'])
-def recipe(recipe_name):
-    return render_template('recipe.html', recipe_name=recipe_name)
+@app.route("/recipes/<string:recipe_id>/", methods=['GET'])
+def recipe(recipe_id):
+    recipe = Recipe.query.filter(Recipe.id == recipe_id).first()
+    return render_template('recipe.html', recipe=recipe)
 
 @app.route("/add_recipe", methods=('GET', 'POST'))
 def add_recipe():
@@ -94,12 +91,12 @@ def add_recipe():
 
 @app.route("/create_shopping_list")
 def create_shopping_list():
-    recipe_list = get_recipes()
+    recipe_list = [(recipe.id, recipe.name) for recipe in Recipe.query.all()]
     return render_template('create_shopping_list.html', recipes=recipe_list)
 
 @app.route("/shopping_list")
 def shopping_list():
-    recipe_list = get_recipes()
+    recipe_list = [(recipe.id, recipe.name) for recipe in Recipe.query.all()]
     return render_template('shopping_list.html', recipes=recipe_list)
 
 if __name__ == "__main__":
